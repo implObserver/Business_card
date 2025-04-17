1cc0d637ce090a94b89789eb7778c0a13132cdc3
implObserver <prognozman@mail.ru>
2025-04-17 22:42:20 +0500

    feat:add my avatar

--- Business_Card/src/entities/myDescription/ui/MyDescription.tsx
+++ Business_Card/src/entities/myDescription/ui/MyDescription.tsx
@@ -1,10 +1,11 @@
 import styles from './styles/MyDescription.module.css'
+import avatar from '@/shared/assets/me.png'
 
 export const MyDescription = () => {
     return (
         <div className={styles.description}>
             <span>Привет. Я профессиональный fullstack web-разработчик. Обожаю свою работу, чистый код и FSD.</span>
-            <img className={styles.avatar} src="src/shared/assets/me.png" alt="" />
+            <img className={styles.avatar} src={avatar} alt="" />
         </div>
     )
 }
